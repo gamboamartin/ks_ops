9c8c93da8ca1bb9f7f55d38d905673b0fc695e63
mgamboa <gamboa.martin@gmail.com>
2024-06-17 10:06:40 -0600

    v0.3.0 Se integra instalacion base de gfrupo

--- __inicializa.php
+++ __inicializa.php
@@ -2,6 +2,7 @@
 
 use base\conexion;
 use gamboamartin\errores\errores;
+use gamboamartin\ks_ops\instalacion\instalacion;
 
 $_SESSION['usuario_id'] = 2;
 
@@ -20,6 +21,19 @@ if(errores::$error){
         $link->rollBack();
     }
     $error = (new errores())->error(mensaje: 'Error al instalar administrador', data: $instala);
+    print_r($error);
+    exit;
+}
+
+
+$ks_ops = new instalacion();
+
+$instala = $ks_ops->instala(link: $link);
+if(errores::$error){
+    if($link->inTransaction()) {
+        $link->rollBack();
+    }
+    $error = (new errores())->error(mensaje: 'Error al instalar instala', data: $instala);
     print_r($error);
     exit;
 }
