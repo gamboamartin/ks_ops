2676ddeff596e6e22c0777de07d2a72258d87dc0
Kevin <kevin.acuna@ivitec.com.mx>
2024-06-24 16:57:21 -0500

    v0.12.0 Se integran catalogo contacto

--- templates/inputs/com_tipo_contacto/modifica.php
+++ templates/inputs/com_tipo_contacto/modifica.php
@@ -1,4 +1,5 @@
 <?php /** @var \gamboamartin\ks_ops\controllers\controlador_com_tipo_contacto $controlador  controlador en ejecucion */ ?>
 <?php use config\views; ?>
+<?php echo $controlador->inputs->codigo; ?>
 <?php echo $controlador->inputs->descripcion; ?>
 <?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
