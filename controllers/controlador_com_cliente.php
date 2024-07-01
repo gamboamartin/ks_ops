ea5a532fa7f0ae4aa43f7b50d60ee7375e219a8b
Kevin <kevin.acuna@ivitec.com.mx>
2024-07-01 18:01:40 -0500

    v0.23.0 Se integran validaciones para asignacion

--- controllers/controlador_com_cliente.php
+++ controllers/controlador_com_cliente.php
@@ -11,10 +11,12 @@ namespace gamboamartin\ks_ops\controllers;
 
 use gamboamartin\errores\errores;
 use gamboamartin\ks_ops\models\com_rel_agente_cliente;
+use gamboamartin\system\actions;
 use stdClass;
 
 final class controlador_com_cliente extends \gamboamartin\comercial\controllers\controlador_com_cliente
 {
+    public string $link_com_rel_agente_cliente_bd = '';
 
     protected function campos_view(): array
     {
@@ -47,9 +49,36 @@ final class controlador_com_cliente extends \gamboamartin\comercial\controllers\
         return $campos_view;
     }
 
+    protected function init_links(): array|string
+    {
+        $links = $this->obj_link->genera_links(controler: $this);
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al generar links', data: $links);
+            print_r($error);
+            exit;
+        }
+
+        $link = $this->obj_link->get_link(seccion: "com_cliente", accion: "asigna_agente_bd");
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al recuperar link autoriza_bd', data: $link);
+            print_r($error);
+            exit;
+        }
+        $this->link_com_rel_agente_cliente_bd = $link;
+
+        return $link;
+    }
+
 
     public function asigna_agente(bool $header, bool $ws = false): array|stdClass
     {
+        $init_links = $this->init_links();
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al inicializar links', data: $init_links);
+            print_r($error);
+            die('Error');
+        }
+
         $this->accion_titulo = 'Asignar agente';
 
         $r_modifica = $this->init_modifica();
@@ -77,6 +106,8 @@ final class controlador_com_cliente extends \gamboamartin\comercial\controllers\
             return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
         }
 
+        $keys_selects['com_agente_id']->required = true;
+
         $base = $this->base_upd(keys_selects: $keys_selects, params: array(), params_ajustados: array());
         if (errores::$error) {
             return $this->retorno_error(mensaje: 'Error al integrar base', data: $base, header: $header, ws: $ws);
@@ -84,4 +115,67 @@ final class controlador_com_cliente extends \gamboamartin\comercial\controllers\
 
         return $r_modifica;
     }
+
+    public function asigna_agente_bd(bool $header, bool $ws = false): array|stdClass
+    {
+        $this->link->beginTransaction();
+
+        $siguiente_view = (new actions())->init_alta_bd();
+        if (errores::$error) {
+            $this->link->rollBack();
+            return $this->retorno_error(mensaje: 'Error al obtener siguiente view', data: $siguiente_view,
+                header: $header, ws: $ws);
+        }
+
+        if (isset($_POST['btn_action_next'])) {
+            unset($_POST['btn_action_next']);
+        }
+
+        $relacion = (new com_rel_agente_cliente(link: $this->link))->filtro_and(filtro: array('com_cliente.id' => $this->registro_id));
+        if (errores::$error) {
+            return $this->retorno_error(mensaje: 'Error al obtener relacion', data: $relacion, header: $header, ws: $ws);
+        }
+
+        $proceso = new stdClass();
+
+        if ($relacion->n_registros == 0) {
+            $registro['com_agente_id'] = $_POST['com_agente_id'];
+            $registro['com_cliente_id'] = $this->registro_id;
+
+            $proceso = (new com_rel_agente_cliente($this->link, array('com_agente')))->alta_registro(registro: $registro);
+            if (errores::$error) {
+                $this->link->rollBack();
+                return $this->retorno_error(mensaje: 'Error al dar de alta relacion', data: $proceso, header: $header,
+                    ws: $ws);
+            }
+
+        } else {
+            $registro['com_agente_id'] = $_POST['com_agente_id'];
+            $registro['com_cliente_id'] = $this->registro_id;
+            $id = $relacion->registros[0]['com_rel_agente_cliente_id'];
+            $proceso = (new com_rel_agente_cliente($this->link, array('com_agente')))->modifica_bd(registro: $registro,id: $id);
+            if (errores::$error) {
+                $this->link->rollBack();
+                return $this->retorno_error(mensaje: 'Error al modificar relacion', data: $proceso, header: $header,
+                    ws: $ws);
+            }
+        }
+
+        $this->link->commit();
+
+        if ($header) {
+            $this->retorno_base(registro_id: $this->registro_id, result: $proceso,
+                siguiente_view: "lista", ws: $ws);
+        }
+        if ($ws) {
+            header('Content-Type: application/json');
+            echo json_encode($proceso, JSON_THROW_ON_ERROR);
+            exit;
+        }
+        $proceso->siguiente_view = "lista";
+
+        return $proceso;
+    }
+
+
 }
--- orm/com_rel_agente_cliente.php
+++ orm/com_rel_agente_cliente.php
@@ -1,17 +1,24 @@
 <?php
-namespace gamboamartin\ks_ops\models;
-use base\orm\_modelo_parent;
-use PDO;
 
-class com_rel_agente_cliente extends _modelo_parent{
-    public function __construct(PDO $link, array $childrens = array()){
+namespace gamboamartin\ks_ops\models;
+
+use base\orm\_modelo_parent;
+use base\orm\_modelo_parent_sin_codigo;
+use gamboamartin\errores\errores;
+use PDO;
+use stdClass;
+
+class com_rel_agente_cliente extends _modelo_parent_sin_codigo
+{
+    public function __construct(PDO $link, array $childrens = array())
+    {
         $tabla = 'com_rel_agente_cliente';
-        $columnas = array($tabla=>false, 'com_agente'=>$tabla, 'com_cliente'=>$tabla);
-        $campos_obligatorios = array('com_agente_id','com_cliente_id');
+        $columnas = array($tabla => false, 'com_agente' => $tabla, 'com_cliente' => $tabla);
+        $campos_obligatorios = array('com_agente_id', 'com_cliente_id');
 
         $columnas_extra = array();
 
-        $atributos_criticos =  array('com_cliente_id','com_agente_id');
+        $atributos_criticos = array('com_cliente_id', 'com_agente_id');
 
         parent::__construct(link: $link, tabla: $tabla, campos_obligatorios: $campos_obligatorios,
             columnas: $columnas, columnas_extra: $columnas_extra, childrens: $childrens,
@@ -20,5 +27,28 @@ class com_rel_agente_cliente extends _modelo_parent{
         $this->NAMESPACE = __NAMESPACE__;
 
         $this->etiqueta = 'Relacion Agente Cliente';
+    }
+
+    public function alta_bd(array $keys_integra_ds = array('codigo', 'descripcion')): array|stdClass
+    {
+        $this->registro = $this->inicializa_campos($this->registro);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al inicializar campo base', data: $this->registro);
+        }
+
+        $r_alta_bd = parent::alta_bd($keys_integra_ds);
+        if (errores::$error) {
+            return $this->error->error(mensaje: 'Error al insertar autorizante', data: $r_alta_bd);
+        }
+        return $r_alta_bd;
+    }
+
+
+    protected function inicializa_campos(array $registros): array
+    {
+        $registros['codigo'] = $this->get_codigo_aleatorio(longitud: 12);
+        $registros['descripcion'] = $registros['codigo'];
+
+        return $registros;
     }
 }
