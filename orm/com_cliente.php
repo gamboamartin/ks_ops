0605475915e228ffde6cceaa4b26c89cb69d448c
mgamboa <gamboa.martin@gmail.com>
2024-07-08 11:27:28 -0600

    v0.30.0 Se integra alta bd base cliente

--- orm/com_cliente.php
+++ orm/com_cliente.php
@@ -1,6 +1,19 @@
 <?php
 namespace gamboamartin\ks_ops\models;
+use gamboamartin\errores\errores;
+use stdClass;
 
 class com_cliente extends \gamboamartin\comercial\models\com_cliente {
+
+    final public function alta_bd(array $keys_integra_ds = array('codigo', 'descripcion')): array|stdClass
+    {
+        $alta_bd = parent::alta_bd(keys_integra_ds: $keys_integra_ds);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al insertar cliente',data:  $alta_bd);
+        }
+        return $alta_bd;
+
+    }
+
 
 }
