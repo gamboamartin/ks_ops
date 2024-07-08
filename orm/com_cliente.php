d51a94bc0bfb6ac335682f068750be6ebb613ac1
mgamboa <gamboa.martin@gmail.com>
2024-07-08 11:29:54 -0600

    v0.31.0 Ser ajusta modelo modifica cliente

--- orm/com_cliente.php
+++ orm/com_cliente.php
@@ -15,5 +15,21 @@ class com_cliente extends \gamboamartin\comercial\models\com_cliente {
 
     }
 
+    final public function modifica_bd(array $registro, int $id, bool $reactiva = false,
+                                      array $keys_integra_ds = array('codigo', 'descripcion'),
+                                      bool $valida_conf_tipo_persona = true,
+                                      bool $valida_metodo_pago = true): array|stdClass
+    {
+        $r_modifica = parent::modifica_bd(registro: $registro, id: $id, reactiva: $reactiva,
+            keys_integra_ds: $keys_integra_ds, valida_conf_tipo_persona: $valida_conf_tipo_persona,
+            valida_metodo_pago: $valida_metodo_pago);
+        
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al modificar cliente',data:  $r_modifica);
+        }
+        return $r_modifica;
+
+    }
+
 
 }
