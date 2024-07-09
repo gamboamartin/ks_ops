d872451637085af566febbaa6576210850b7e59e
Kevin <kevin.acuna@ivitec.com.mx>
2024-07-09 12:32:29 -0500

    v0.39.0 Se ajusta hijos en agentes

--- controllers/controlador_com_agente.php
+++ controllers/controlador_com_agente.php
@@ -8,9 +8,18 @@
  */
 namespace gamboamartin\ks_ops\controllers;
 
+use gamboamartin\template\html;
+use PDO;
 use stdClass;
 
 final class controlador_com_agente extends \gamboamartin\comercial\controllers\controlador_com_agente {
+
+    public function __construct(PDO      $link, html $html = new \gamboamartin\template_1\html(),
+                                stdClass $paths_conf = new stdClass())
+    {
+        parent::__construct(link: $link, html: $html, paths_conf: $paths_conf);
+        $this->childrens_data = array();
+    }
 
     public function init_datatable(): stdClass
     {
