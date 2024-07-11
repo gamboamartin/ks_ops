ae385eb55241da35fbd2f0a5d6371c3df4f65311
Kevin <kevin.acuna@ivitec.com.mx>
2024-07-11 12:38:10 -0500

    v0.43.0 Se integran documentos

---
+++ controllers/controlador_doc_documento.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace gamboamartin\ks_ops\controllers;
+
+class controlador_doc_documento extends \gamboamartin\comercial\controllers\controlador_doc_documento {}
