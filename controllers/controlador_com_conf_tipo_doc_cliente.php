d206f24605f6b41b9f5cc9e175c3c62da4958351
Kevin <kevin.acuna@ivitec.com.mx>
2024-07-15 15:18:58 -0500

    v0.48.0 Se integran cat. para secciones de documentos

---
+++ controllers/controlador_com_conf_tipo_doc_cliente.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace gamboamartin\ks_ops\controllers;
+
+class controlador_com_conf_tipo_doc_cliente extends \gamboamartin\comercial\controllers\controlador_com_conf_tipo_doc_cliente {
+}
