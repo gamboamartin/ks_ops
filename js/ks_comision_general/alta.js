3b6eea251ed4999452b3726f0e437c7d52010e0f
Kevin <kevin.acuna@ivitec.com.mx>
2024-07-19 14:25:35 -0500

    v0.58.0 Se integra validaciones por js

---
+++ js/ks_comision_general/alta.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var fecha_inicio = document.getElementById('fecha_inicio').value;
+    var fecha_fin = document.getElementById('fecha_fin').value;
+    document.getElementById('fecha_fin').setAttribute('min', fecha_inicio);
+    document.getElementById('fecha_inicio').setAttribute('max', fecha_fin);
+});
+
+document.getElementById('fecha_inicio').addEventListener('change', function() {
+    var fecha_inicio = this.value;
+    document.getElementById('fecha_fin').setAttribute('min', fecha_inicio);
+});
+
+document.getElementById('fecha_fin').addEventListener('change', function() {
+    var fecha_fin = this.value;
+    document.getElementById('fecha_inicio').setAttribute('max', fecha_fin);
+});
