407dee91cedcf3ae9d26fc9a5f497b37853ffe40
Kevin <kevin.acuna@ivitec.com.mx>
2024-07-22 11:43:41 -0500

    v0.64.0 Se integra cat  empleado

---
+++ templates/inputs/em_empleado/modifica.php
@@ -0,0 +1,31 @@
+<?php /** @var  \gamboamartin\empleado\controllers\controlador_em_empleado $controlador  controlador en ejecucion */ ?>
+<?php use config\views; ?>
+<?php echo $controlador->inputs->nombre; ?>
+<?php echo $controlador->inputs->telefono; ?>
+<?php echo $controlador->inputs->ap; ?>
+<?php echo $controlador->inputs->am; ?>
+<?php echo $controlador->inputs->dp_pais_id; ?>
+<?php echo $controlador->inputs->dp_estado_id; ?>
+<?php echo $controlador->inputs->dp_municipio_id; ?>
+<?php echo $controlador->inputs->dp_cp_id; ?>
+<?php echo $controlador->inputs->dp_colonia_postal_id; ?>
+<?php echo $controlador->inputs->dp_calle_pertenece_id; ?>
+<?php echo $controlador->inputs->cat_sat_regimen_fiscal_id; ?>
+<?php echo $controlador->inputs->org_puesto_id; ?>
+<?php echo $controlador->inputs->cat_sat_tipo_regimen_nom_id; ?>
+<?php echo $controlador->inputs->cat_sat_tipo_jornada_nom_id; ?>
+<?php echo $controlador->inputs->rfc; ?>
+<?php echo $controlador->inputs->nss; ?>
+<?php echo $controlador->inputs->curp; ?>
+<?php echo $controlador->inputs->em_registro_patronal_id; ?>
+<?php echo $controlador->inputs->em_centro_costo_id; ?>
+<?php echo $controlador->inputs->salario_diario; ?>
+<?php echo $controlador->inputs->salario_diario_integrado; ?>
+<?php echo $controlador->inputs->salario_total; ?>
+<?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
+<div class="col-row-12">
+    <?php foreach ($controlador->buttons as $button){ ?>
+        <?php echo $button; ?>
+    <?php }?>
+</div>
+
