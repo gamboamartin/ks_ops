5b7faa265c2b7dea91f2cc0a44a8a28e420e354e
Kevin <kevin.acuna@ivitec.com.mx>
2024-07-25 11:45:40 -0500

    v0.85.1 Se integra tipo

---
+++ templates/inputs/org_tipo_puesto/modifica.php
@@ -0,0 +1,7 @@
+<?php /** @var \gamboamartin\ks_ops\controllers\controlador_org_tipo_puesto $controlador  controlador en ejecucion */ ?>
+<?php use config\views; ?>
+<?php echo $controlador->inputs->codigo; ?>
+<?php echo $controlador->inputs->descripcion; ?>
+<?php include (new views())->ruta_templates.'botons/submit/modifica_bd.php';?>
+
+
