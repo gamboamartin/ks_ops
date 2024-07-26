9a604b1f9b9b25f60969e89a72e762bed24e90f0
Kevin <kevin.acuna@ivitec.com.mx>
2024-07-26 15:25:06 -0500

    v0.92.1 Se integra preload por rangos de fechas

--- js/ks_comision_general/detalle_comision.js
+++ js/ks_comision_general/detalle_comision.js
@@ -4,6 +4,16 @@ let sl_com_agente = $("#com_agente_id");
 var date_fecha_inicio = document.getElementById('fecha_inicio');
 var date_fecha_fin = document.getElementById('fecha_fin');
 
+document.addEventListener('DOMContentLoaded', () => {
+    var fecha_inicio = document.getElementById('fecha_inicio').value;
+    var fecha_fin = document.getElementById('fecha_fin').value;
+
+    document.getElementById('fecha_inicio').setAttribute('min', fecha_inicio);
+    document.getElementById('fecha_inicio').setAttribute('max', fecha_fin);
+    document.getElementById('fecha_fin').setAttribute('min', fecha_inicio);
+    document.getElementById('fecha_fin').setAttribute('max', fecha_fin);
+});
+
 function asigna_datos(com_agente_id) {
 
     let url = get_url("ks_detalle_comision", "get_ultimo_registro", {nombre_campo: 'com_agente_id', valor: com_agente_id});
@@ -13,7 +23,8 @@ function asigna_datos(com_agente_id) {
             return;
         }
 
-        console.log(data);
+        let fecha_inicio_general = new Date(data.data.ks_comision_general_fecha_inicio);
+        let fecha_fin_general = new Date(data.data.ks_comision_general_fecha_fin);
         let fecha_inicio = new Date(data.data.ks_detalle_comision_fecha_fin);
         let fecha_fin = new Date(data.data.ks_detalle_comision_fecha_inicio);
 
@@ -21,12 +32,18 @@ function asigna_datos(com_agente_id) {
 
         let fecha_inicio_formateada = fecha_inicio.toISOString().split('T')[0];
         let fecha_fin_formateada = fecha_fin.toISOString().split('T')[0];
+        let fecha_inicio_general_formateada = fecha_inicio_general.toISOString().split('T')[0];
+        let fecha_fin_general_formateada = fecha_fin_general.toISOString().split('T')[0];
 
         date_fecha_fin.value = fecha_fin_formateada;
         date_fecha_inicio.value = fecha_inicio_formateada;
+        date_fecha_inicio_general.value = fecha_inicio_general_formateada;
+        date_fecha_inicio_general.value = fecha_fin_general_formateada;
 
+        //document.getElementById('fecha_inicio').setAttribute('max', date_fecha_fin.value);
+        document.getElementById('fecha_inicio').setAttribute('min', date_fecha_inicio_general.value);
         document.getElementById('fecha_fin').setAttribute('min', date_fecha_inicio.value);
-        document.getElementById('fecha_inicio').setAttribute('max', date_fecha_fin.value);
+        document.getElementById('fecha_fin').setAttribute('max', date_fecha_inicio_general.value);
     });
 }
 
