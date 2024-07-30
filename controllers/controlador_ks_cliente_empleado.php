7bf44a52d605bb88f231211a502908dbe56250ff
Kevin <kevin.acuna@ivitec.com.mx>
2024-07-30 17:30:34 -0500

    v0.104.1 Se integra controlador

---
+++ controllers/controlador_ks_cliente_empleado.php
@@ -0,0 +1,195 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace gamboamartin\ks_ops\controllers;
+
+use base\controller\controler;
+use gamboamartin\errores\errores;
+use gamboamartin\ks_ops\html\ks_cliente_empleado_html;
+use gamboamartin\ks_ops\models\ks_cliente_empleado;
+use gamboamartin\system\_ctl_base;
+use gamboamartin\system\links_menu;
+use gamboamartin\template\html;
+use PDO;
+use stdClass;
+final class controlador_ks_cliente_empleado extends _ctl_base  {
+
+    public function __construct(PDO      $link, html $html = new \gamboamartin\template_1\html(),
+                                stdClass $paths_conf = new stdClass())
+    {
+        $modelo = new ks_cliente_empleado(link: $link);
+        $html = new ks_cliente_empleado_html(html: $html);
+        $obj_link = new links_menu(link: $link, registro_id: $this->registro_id);
+
+        $datatables = $this->init_datatable();
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al inicializar datatable', data: $datatables);
+            print_r($error);
+            die('Error');
+        }
+
+        parent::__construct(html: $html, link: $link, modelo: $modelo, obj_link: $obj_link, datatables: $datatables,
+            paths_conf: $paths_conf);
+
+        $init_controladores = $this->init_controladores(paths_conf: $paths_conf);
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al inicializar controladores',data:  $init_controladores);
+            print_r($error);
+            die('Error');
+        }
+
+        $configuraciones = $this->init_configuraciones();
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al inicializar configuraciones', data: $configuraciones);
+            print_r($error);
+            die('Error');
+        }
+
+        $init_links = $this->init_links();
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al inicializar links', data: $init_links);
+            print_r($error);
+            die('Error');
+        }
+
+    }
+
+    public function alta(bool $header, bool $ws = false): array|string
+    {
+        $r_alta = $this->init_alta();
+        if (errores::$error) {
+            return $this->retorno_error(mensaje: 'Error al inicializar alta', data: $r_alta, header: $header, ws: $ws);
+        }
+
+        $keys_selects = $this->init_selects_inputs();
+        if (errores::$error) {return $this->errores->error(mensaje: 'Error al inicializar selects', data: $keys_selects);
+        }
+
+        $inputs = $this->inputs(keys_selects: $keys_selects);
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al obtener inputs', data: $inputs);
+        }
+
+        return $r_alta;
+    }
+
+    protected function campos_view(): array
+    {
+        $keys = new stdClass();
+        $keys->inputs = array('codigo');
+        $keys->fechas = array();
+        $keys->selects = array();
+
+        $init_data = array();
+        $init_data['com_cliente'] = "gamboamartin\\comercial";
+        $init_data['em_empleado'] = "gamboamartin\\empleado";
+        $campos_view = $this->campos_view_base(init_data: $init_data, keys: $keys);
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al inicializar campo view', data: $campos_view);
+        }
+
+        return $campos_view;
+    }
+
+    private function init_configuraciones(): controler
+    {
+        $this->titulo_lista = 'Registro de clientes y empleados';
+
+        return $this;
+    }
+
+    private function init_controladores(stdClass $paths_conf): controler
+    {
+        return $this;
+    }
+
+    protected function init_links(): array|stdClass
+    {
+        $links = $this->obj_link->genera_links(controler: $this);
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al generar links', data: $links);
+            print_r($error);
+            exit;
+        }
+
+        return $links;
+    }
+
+    private function init_selects(array $keys_selects, string $key, string $label, int|null $id_selected = -1, int $cols = 6,
+                                  bool  $con_registros = true, array $filtro = array(), array $columns_ds =  array()): array
+    {
+        $keys_selects = $this->key_select(cols: $cols, con_registros: $con_registros, filtro: $filtro, key: $key,
+            keys_selects: $keys_selects, id_selected: $id_selected, label: $label, columns_ds: $columns_ds);
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
+
+        return $keys_selects;
+    }
+
+    public function init_selects_inputs(): array{
+
+        $keys_selects = $this->init_selects(keys_selects: array(), key: "com_cliente_id", label: "Cliente",
+            cols: 8,columns_ds: array('com_cliente_razon_social'));
+        if(errores::$error){
+            return $this->errores->error(mensaje: 'Error al integrar selector',data:  $keys_selects);
+        }
+
+        $keys_selects = $this->init_selects(keys_selects: $keys_selects, key: "em_empleado_id", label: "empleado",
+            cols: 8,columns_ds: array('em_empleado_nombre_completo'));
+        if(errores::$error){
+            return $this->errores->error(mensaje: 'Error al integrar selector',data:  $keys_selects);
+        }
+
+        return $keys_selects;
+    }
+
+    final public function init_datatable(): stdClass
+    {
+        $datatables = new stdClass();
+        $datatables->columns = array();
+        $datatables->columns['ks_cliente_empleado_id']['titulo'] = 'Id';
+        $datatables->columns['com_cliente_rfc']['titulo'] = 'RFC';
+        $datatables->columns['com_cliente_razon_social']['titulo'] = 'Cliente';
+        $datatables->columns['em_empleado_nombre_completo']['titulo'] = 'Empleado';
+
+        $datatables->filtro = array();
+        $datatables->filtro[] = 'ks_cliente_empleado.id';
+        $datatables->filtro[] = 'com_cliente.codigo';
+        $datatables->filtro[] = 'com_cliente.rfc';
+        $datatables->filtro[] = 'com_cliente.razon_social';
+        $datatables->filtro[] = 'em_empleado.nombre_completo';
+
+        return $datatables;
+    }
+
+    public function modifica(bool $header, bool $ws = false): array|stdClass
+    {
+        $r_modifica = $this->init_modifica();
+        if (errores::$error) {
+            return $this->retorno_error(
+                mensaje: 'Error al generar salida de template', data: $r_modifica, header: $header, ws: $ws);
+        }
+
+        $keys_selects = $this->init_selects_inputs();
+        if (errores::$error) {
+            return $this->retorno_error(mensaje: 'Error al inicializar selects', data: $keys_selects, header: $header,
+                ws: $ws);
+        }
+
+        $keys_selects['com_cliente_id']->id_selected = $this->registro['com_cliente_id'];
+        $keys_selects['em_empleado_id']->id_selected = $this->registro['em_empleado_id'];
+
+        $base = $this->base_upd(keys_selects: $keys_selects, params: array(), params_ajustados: array());
+        if (errores::$error) {
+            return $this->retorno_error(mensaje: 'Error al integrar base', data: $base, header: $header, ws: $ws);
+        }
+
+        return $r_modifica;
+    }
+}
---
+++ templates/directivas/ks_cliente_empleado_html.php
@@ -0,0 +1,11 @@
+<?php
+namespace gamboamartin\ks_ops\html;
+
+use gamboamartin\errores\errores;
+use gamboamartin\ks_ops\models\ks_cliente;
+use gamboamartin\system\html_controler;
+use PDO;
+
+
+class ks_cliente_empleado_html extends html_controler {
+}
