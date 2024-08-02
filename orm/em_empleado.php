674b38acc79de70997ee5318f921f699100ea0e9
Kevin <kevin.acuna@ivitec.com.mx>
2024-08-02 17:47:43 -0500

    v0.119.1 Se sobrescribe modelo

--- orm/em_empleado.php
+++ orm/em_empleado.php
@@ -2,11 +2,76 @@
 namespace gamboamartin\ks_ops\models;
 use gamboamartin\cat_sat\models\cat_sat_tipo_persona;
 use gamboamartin\direccion_postal\models\dp_municipio;
+use gamboamartin\empleado\models\em_registro_patronal;
 use gamboamartin\errores\errores;
 use PDO;
 use stdClass;
 
 class em_empleado extends \gamboamartin\empleado\models\em_empleado {
+
+    public function __construct(PDO $link)
+    {
+        parent::__construct(link: $link);
+        $this->extension_estructura['ks_empleado']['key'] = 'em_empleado_id';
+        $this->extension_estructura['ks_empleado']['enlace'] = 'em_empleado';
+        $this->extension_estructura['ks_empleado']['key_enlace'] = 'id';
+    }
+
+    final public function alta_bd(array $keys_integra_ds = array('codigo', 'descripcion')): array|stdClass
+    {
+        $predefinidos = $this->valores_predeterminados();
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al obtener valores predeterminados',data:  $predefinidos);
+        }
+
+        $registro_original = $this->registro;
+        $alta_bd = parent::alta_bd(keys_integra_ds: $keys_integra_ds);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al insertar cliente',data:  $alta_bd);
+        }
+
+        if(isset($registro_original['registro_patronal'])){
+            $ks_empleado_ins = $this->ks_empleado_ins(em_empleado_id: $alta_bd->registro_id, registros: $registro_original);
+            if(errores::$error){
+                return $this->error->error(mensaje: 'Error al inicializar valores',data:  $ks_empleado_ins);
+            }
+
+            $ks_empleado_alta = (new ks_empleado(link: $this->link))->alta_registro(registro: $ks_empleado_ins);
+            if(errores::$error){
+                return $this->error->error(mensaje: 'Error al insertar ks_cliente',data:  $ks_empleado_alta);
+            }
+        }
+
+        return $alta_bd;
+    }
+
+    public function valores_predeterminados() : array
+    {
+        $this->registro['em_registro_patronal_id'] = (new em_registro_patronal(link: $this->link))->id_predeterminado();
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al obtener id registro patronal predeterminado', data: $this->registro);
+        }
+
+        return $this->registro;
+    }
+
+    private function ks_empleado_ins(int $em_empleado_id, array $registros): array
+    {
+        $empleado = (new em_empleado(link: $this->link))->registro(registro_id: $em_empleado_id);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al obtener registro empleado',data: $empleado);
+        }
+
+        $ks_empleado_ins['em_empleado_id'] = $em_empleado_id;
+        $ks_empleado_ins['codigo'] = $empleado['em_empleado_codigo'];
+        $ks_empleado_ins['descripcion'] = $empleado['em_empleado_descripcion'];
+        $ks_empleado_ins['descripcion_select'] = $empleado['em_empleado_descripcion_select'];
+        $ks_empleado_ins['alias'] = $empleado['em_empleado_alias'];
+        $ks_empleado_ins['codigo_bis'] = $empleado['em_empleado_codigo_bis'];
+        $ks_empleado_ins['registro_patronal'] = $registros['registro_patronal'];
+
+        return $ks_empleado_ins;
+    }
 
     public function transacciona_em_rel_empleado_sucursal(array $data, int $em_empleado_id): array|stdClass {
         return array();
