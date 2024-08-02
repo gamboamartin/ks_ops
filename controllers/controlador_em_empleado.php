a20f1fc47b3dd9532aedd398df001ae69a6722dc
Kevin <kevin.acuna@ivitec.com.mx>
2024-08-02 17:05:57 -0500

    v0.118.1 Se cambia input

--- controllers/controlador_em_empleado.php
+++ controllers/controlador_em_empleado.php
@@ -25,6 +25,42 @@ final class controlador_em_empleado extends \gamboamartin\empleado\controllers\c
         $this->childrens_data = array();
     }
 
+    protected function campos_view(): array
+    {
+        $keys = new stdClass();
+        $keys->inputs = array('codigo', 'descripcion', 'nombre', 'ap', 'am',  'rfc', 'curp', 'nss', 'salario_diario',
+            'salario_diario_integrado','com_sucursal','org_sucursal', 'salario_total', 'numero_exterior', 'numero_interior',
+            'registro_patronal');
+        $keys->telefonos = array('telefono');
+        $keys->fechas = array('fecha_inicio_rel_laboral', 'fecha_inicio', 'fecha_final');
+        $keys->emails = array('correo');
+        $keys->selects = array();
+
+        $init_data = array();
+        $init_data['dp_pais'] = "gamboamartin\\direccion_postal";
+        $init_data['dp_estado'] = "gamboamartin\\direccion_postal";
+        $init_data['dp_municipio'] = "gamboamartin\\direccion_postal";
+        $init_data['dp_cp'] = "gamboamartin\\direccion_postal";
+        $init_data['dp_colonia_postal'] = "gamboamartin\\direccion_postal";
+        $init_data['dp_calle_pertenece'] = "gamboamartin\\direccion_postal";
+        $init_data['cat_sat_regimen_fiscal'] = "gamboamartin\\cat_sat";
+        $init_data['cat_sat_tipo_regimen_nom'] = "gamboamartin\\cat_sat";
+        $init_data['cat_sat_tipo_jornada_nom'] = "gamboamartin\\cat_sat";
+        $init_data['org_puesto'] = "gamboamartin\\organigrama";
+        $init_data['em_centro_costo'] = "gamboamartin\\empleado";
+        $init_data['em_empleado'] = "gamboamartin\\empleado";
+        $init_data['em_registro_patronal'] = "gamboamartin\\empleado";
+        $init_data['com_sucursal'] = "gamboamartin\\comercial";
+
+
+        $campos_view = $this->campos_view_base(init_data: $init_data, keys: $keys);
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al inicializar campo view', data: $campos_view);
+        }
+
+        return $campos_view;
+    }
+
     public function init_selects_inputs(): array
     {
         $keys_selects = parent::init_selects_inputs();
@@ -43,6 +79,12 @@ final class controlador_em_empleado extends \gamboamartin\empleado\controllers\c
         $keys_selects['rfc']->cols = 4;
         $keys_selects['curp']->cols = 4;
         $keys_selects['nss']->cols = 4;
+
+        $keys_selects = (new \base\controller\init())->key_select_txt(cols: 12, key: 'registro_patronal',
+            keys_selects: $keys_selects, place_holder: 'Registro Patronal');
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
 
         return $keys_selects;
     }
