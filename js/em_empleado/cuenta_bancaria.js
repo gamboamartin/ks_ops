158bf63ad5dd4c9931d49f636bf62032de51ceda
Kevin <kevin.acuna@ivitec.com.mx>
2024-08-07 13:38:39 -0500

    v0.136.1 Se integra reformateo en mascara

--- js/em_empleado/cuenta_bancaria.js
+++ js/em_empleado/cuenta_bancaria.js
@@ -1,23 +1,33 @@
 $('#em_cuenta_bancaria').DataTable();
 
 let sl_bn_sucursal = $("#bn_sucursal_id");
-let txt_clabe = document.getElementById('clabe');
+let mask;
 
 sl_bn_sucursal.change(function () {
     let option = $(this).find("option:selected");
-    let codigo = option.data("bn_sucursal_codigo");
+    let codigo = option.data("bn_sucursal_codigo").toString();
 
-    let formato = codigo.toString().split('').map(char => char === '0' ? `\\${char}` : char).join('');
-    let mascara = `${formato}-000000000000000000`;
+    let formato = codigo.split('').map(char => char === '0' ? `\\${char}` : char).join('');
+    let mascara = `${formato}000000000000000000`;
 
-    if (txt_clabe.inputmask) {
-        txt_clabe.inputmask.remove();
+    let txt_clabe = document.getElementById('clabe');
+    let valor_actual = txt_clabe.value;
+
+    if (valor_actual.length > 3) {
+        valor_actual = codigo + valor_actual.slice(3);
+    } else {
+        valor_actual = codigo;
     }
 
-    let mask = IMask(txt_clabe, {
-        mask:[{ mask: '' }, { mask: mascara }]
+    if (mask) {
+        mask.destroy();
+    }
+
+    mask = IMask(txt_clabe, {
+        mask: mascara
     });
 
+    txt_clabe.value = valor_actual;
     mask.updateValue();
 });
 
