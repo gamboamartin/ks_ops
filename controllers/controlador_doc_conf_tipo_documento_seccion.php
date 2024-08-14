4a5f3842b106474c59020eb505f7abf68d8ed1f0
Kevin <kevin.acuna@ivitec.com.mx>
2024-08-14 15:40:25 -0500

    v0.150.1 Se extiende cat. conf de base

---
+++ controllers/controlador_doc_conf_tipo_documento_seccion.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace gamboamartin\ks_ops\controllers;
+
+class controlador_doc_conf_tipo_documento_seccion extends \gamboamartin\documento\controllers\controlador_doc_conf_tipo_documento_seccion {
+}
