a1cbce0672538af34c5fb53ce15914644be0dcab
Kevin <kevin.acuna@ivitec.com.mx>
2024-08-30 10:56:59 -0500

    v0.162.1 Se integran conf base

---
+++ views/com_cliente/leer_qr.php
@@ -0,0 +1,50 @@
+<?php /** @var  \gamboamartin\comercial\controllers\controlador_com_cliente $controlador  controlador en ejecucion */ ?>
+<?php use config\views; ?>
+
+<main class="main section-color-primary">
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-lg-12">
+
+                <div class="widget  widget-box box-container form-main widget-form-cart" id="form">
+
+                    <form method="post" action="<?php echo $controlador->link_com_cliente_leer_qr; ?>"
+                          class="form-additional" enctype="multipart/form-data">
+
+                    <?php include (new views())->ruta_templates."head/title.php"; ?>
+                    <?php include (new views())->ruta_templates."head/subtitulo.php"; ?>
+                    <?php include (new views())->ruta_templates."mensajes.php"; ?>
+
+                    <?php echo $controlador->inputs->documento; ?>
+                    <?php echo $controlador->inputs->btn_action_next; ?>
+                    <?php echo $controlador->inputs->id_retorno; ?>
+                    <?php echo $controlador->inputs->seccion_retorno; ?>
+
+                    <?php include (new views())->ruta_templates.'botons/submit/alta_bd.php';?>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+
+</main>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
