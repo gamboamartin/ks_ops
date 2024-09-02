7b443ac4811d66a19b47ae98aba18f4056636398
Kevin <kevin.acuna@ivitec.com.mx>
2024-09-02 17:42:15 -0500

    v0.168.1 Se integra prospectos

---
+++ controllers/controlador_com_prospecto.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace gamboamartin\ks_ops\controllers;
+
+use base\controller\init;
+use gamboamartin\errores\errores;
+
+final class controlador_com_prospecto extends \gamboamartin\comercial\controllers\controlador_com_prospecto  {
+
+    public function init_selects_inputs(): array
+    {
+        $keys_selects = parent::init_selects_inputs();
+        $keys_selects['com_tipo_prospecto_id']->cols = 12;
+        $keys_selects['com_agente_id']->cols = 12;
+
+        $keys_selects = (new init())->key_select_txt(cols: 12, key: 'nombre',
+            keys_selects: $keys_selects, place_holder: 'Nombre');
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
+        $keys_selects = (new init())->key_select_txt(cols: 6, key: 'apellido_paterno',
+            keys_selects: $keys_selects, place_holder: 'Apellido Paterno');
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
+        $keys_selects = (new init())->key_select_txt(cols: 6, key: 'apellido_materno',
+            keys_selects: $keys_selects, place_holder: 'Apellido Materno');
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
+        $keys_selects = (new init())->key_select_txt(cols: 4, key: 'telefono',
+            keys_selects: $keys_selects, place_holder: 'Tel');
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
+        $keys_selects = (new init())->key_select_txt(cols: 8, key: 'correo',
+            keys_selects: $keys_selects, place_holder: 'Correo');
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
+        $keys_selects = (new init())->key_select_txt(cols: 12, key: 'razon_social',
+            keys_selects: $keys_selects, place_holder: 'Razón Social');
+        if (errores::$error) {
+            return $this->errores->error(mensaje: 'Error al maquetar key_selects', data: $keys_selects);
+        }
+
+
+
+        return $keys_selects;
+
+    }
+}
