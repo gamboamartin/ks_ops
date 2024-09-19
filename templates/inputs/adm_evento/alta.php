8ebef3fe9a09e64bdf22e54376e735ddf4fcc578
Kevin <kevin.acuna@ivitec.com.mx>
2024-09-19 16:14:19 -0500

    v0.177.2 Se integran cat. heredado para eventos

---
+++ templates/inputs/adm_evento/alta.php
@@ -0,0 +1,10 @@
+<?php /** @var gamboamartin\acl\controllers\controlador_adm_evento $controlador  controlador en ejecucion */ ?>
+<?php use config\views; ?>
+
+<?php echo $controlador->inputs->adm_calendario_id; ?>
+<?php echo $controlador->inputs->titulo; ?>
+<?php echo $controlador->inputs->fecha_inicio; ?>
+<?php echo $controlador->inputs->fecha_fin; ?>
+<?php echo $controlador->inputs->descripcion; ?>
+
+<?php include (new views())->ruta_templates.'botons/submit/alta_bd.php';?>
