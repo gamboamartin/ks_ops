065ef75ab2bc233bdfdf203919ff4574f579eb79
Kevin <kevin.acuna@ivitec.com.mx>
2024-10-07 13:42:28 -0500

    v0.187.2 Se integran validaciones

--- js/com_prospecto/etapa.js
+++ js/com_prospecto/etapa.js
@@ -31,3 +31,38 @@ const fecha_actual = hoy.toISOString().split('T')[0];
 txt_fecha_inicio.value = fecha_actual;
 txt_fecha_fin.value = fecha_actual;
 
+document.addEventListener('DOMContentLoaded', () => {
+    var fecha_inicio = document.getElementById('fecha_inicio').value;
+    var fecha_fin = document.getElementById('fecha_fin').value;
+    document.getElementById('fecha_fin').setAttribute('min', fecha_inicio);
+    document.getElementById('fecha_inicio').setAttribute('max', fecha_fin);
+});
+
+document.getElementById('fecha_inicio').addEventListener('change', function() {
+    var fecha_inicio = this.value;
+    document.getElementById('fecha_fin').setAttribute('min', fecha_inicio);
+});
+
+document.getElementById('fecha_fin').addEventListener('change', function() {
+    var fecha_fin = this.value;
+    document.getElementById('fecha_inicio').setAttribute('max', fecha_fin);
+});
+
+document.getElementById('hora_inicio').addEventListener('change', validar_horas);
+document.getElementById('hora_fin').addEventListener('change', validar_horas);
+
+function validar_horas() {
+    var horaInicio = document.getElementById('hora_inicio').value;
+    var horaFin = document.getElementById('hora_fin').value;
+
+    if (horaInicio && horaFin) {
+        if (horaInicio > horaFin) {
+            alert('La hora de inicio no puede ser posterior a la hora de fin.');
+            document.getElementById('hora_inicio').value = '';
+        } else if (horaFin < horaInicio) {
+            alert('La hora de fin no puede ser anterior a la hora de inicio.');
+            document.getElementById('hora_fin').value = '';
+        }
+    }
+}
+
