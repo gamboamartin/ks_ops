7c06b621bc17a9496ad2723efcec9b031e822f52
MauricioHdz <mauricioisraelhernandez@gmail.com>
2024-11-05 11:59:15 -0600

    v0.192.4 Se ajusta elemento de com_prospecto etapa

---
+++ controllers/controlador_com_prospecto_etapa.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Martin Gamboa Vazquez
+ * @version 1.0.0
+ * @created 2022-05-14
+ * @final En proceso
+ *
+ */
+namespace gamboamartin\ks_ops\controllers;
+
+use base\controller\init;
+use gamboamartin\errores\errores;
+
+final class controlador_com_prospecto_etapa extends \gamboamartin\comercial\controllers\controlador_com_prospecto_etapa  {
+}
